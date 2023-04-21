a592d3f9189f84001e7c82506ca56617615c0bcf
Diava0314 <juan14diavanera@gamil.com>
2023-04-20 23:39:36 -0500

    ejercio

---
+++ src/e16.ts
@@ -0,0 +1,10 @@
+/**realizar un formulario que contenga:
+ * 1) Un dropdown el cual debe mostrar dos opciones::
+ * - convertir de grados Cº a Fº
+ * - convertir de Fº a Cº
+ * 2) un input para escribir la cantidad numerica a convertir
+ * 3) un boton para calcular la conversion.
+ *
+ * recordar que la conversion debe hacerse cuando se envia el formulario
+ * no cuando se hace click en el boton.
+ * */
